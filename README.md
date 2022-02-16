50f080e5748bce16138245dbd9ee4a46eebd43d1
KWY0218 <wodrk1001@gmail.com>
2022-02-16 15:53:40 +0900

    AndroidCompoenets-Activity

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# AndroidLoadMap
+## Activity란?
+
+- Activity는 사용자와 상호작용할 수 있는 사용자 인터페이스(UI)를 포함한 화면을 말한다.
+
+## Activity 생명 주기
+
+Activity 시스템은 stack으로 관리된다.
+
+### Activity Diagram
+
+![Untitled](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/2349645c-85cc-4447-94c1-779f11198fb6/Untitled.png)
+
+- 처음 앱 시작할 때
+
+onCreate → onStart → onResume
+
+- 잠시 앱 자리비울 때
+
+onPause → onStop → (재접속) → onRestart → onResume
+
+- 앱 종료할 때
+
+onPause → onStop → onDestory
+
+3가지 핵심 루프
+
+- Activity의 전체 생명주기는 첫 호출인 onCreate() 와 마지막 호출인 onDestroy() 사이에서 일어난다. Activity는 onCreate()에서 전역 상태를 설정하고, onDestroy()에서 나머지 모든 리소스를 해체한다.
+- Activity의 가시적 생명주기는 onStart() 호출과 onStop() 호출 사이에 발생한다. 이 시간 동안 사용자는 화면에서 activity를 볼 수 있지만 foreground에 있지 않고 사용자와 상호 작용할 수 있다. onStart() 와 onStop() 메서드는 Activity가 사용자에게 표시되고 숨겨짐에 따라 여러번 호출될 수 있다.
+- Activity의 foreground 생명주기는 onResume()과 onPause()호출 사이에 발생한다. 이 시간 동안 Activity는 사용자가 볼 수 있고, 사용자와 상호작용할 수 있다. Activity는 onResume()과 onPause() 사이를 자주 이동할 수 있다.
+
+Activity의 전체 생명 주기는 다음 Activity 메서드에 의해 정의된다.
--- app/src/main/java/com/example/androidloadmap/MainActivity.kt
+++ app/src/main/java/com/example/androidloadmap/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.androidloadmap
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -16,7 +20,46 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Box(Modifier.fillMaxWidth()) {
+                Button(
+                    onClick = {
+                        Log.d("MainActivityTAG", "Btn Click")
+                    }
+                ) {
+                    Text(text = "BTN")
+                }
+            }
         }
+        Log.d("MainActivityTAG", "onCreate")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("MainActivityTAG", "onRestart")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivityTAG", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivityTAG", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivityTAG", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivityTAG", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivityTAG", "onDestroy")
     }
 }
