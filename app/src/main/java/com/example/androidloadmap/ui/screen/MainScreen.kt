fb8e5959c7b58a789bb628bcd9f781d5b70513ff
KWY0218 <wodrk1001@gmail.com>
2022-02-24 14:20:11 +0900

    Add Screen

---
+++ app/src/main/java/com/example/androidloadmap/ui/screen/MainScreen.kt
@@ -0,0 +1,15 @@
+package com.example.androidloadmap.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+    }
+}
